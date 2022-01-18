43051bb2878f4d12edaf4286af47867f2a57beaf
unknown <rautanokka@gmail.com>
2022-01-18 19:41:47 +0200

    Historia lisätty laskimeen

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, TextInput, FlatList } from 'react-native';
 
 
 export default function App() {
   const [firstNum, setFirstNum] = useState(0)
   const [secondNum, setSecondNum] = useState(0)
-  const [result, setResult] = useState(null)
+  const [result, setResult] = useState(0)
+  const [data, setData] = useState([]);
+
+  const calculate = (operator) => {
+    switch (operator) {
+      case "+":
+        let plusResult = Number(firstNum) + Number(secondNum)
+        console.log(plusResult)
+        setResult(plusResult)
+        setData(data.concat({ calculation: `${firstNum} ${operator} ${secondNum} = ${plusResult}` }))
+        break;
+      case "-":
+        let minusResult = Number(firstNum) - Number(secondNum)
+        setResult(minusResult)
+        setData(data.concat({ calculation: `${firstNum} ${operator} ${secondNum} = ${minusResult}` }))
+        break;
+    }
+  }
 
 
   return (
 
     <View style={styles.container}>
-      <Text>{result ? `Result: ${result}` : ""}</Text>
+      <Text style={styles.result}>{result ? `Result: ${result}` : ""}</Text>
       <TextInput style={styles.numberinput} keyboardType={"numeric"} onChangeText={text => setFirstNum(text)} />
       <TextInput style={styles.numberinput} keyboardType={"numeric"} onChangeText={text => setSecondNum(text)} />
       <View style={styles.buttoncontainer}>
-        <Button title="+" onPress={() => setResult(Number(firstNum) + Number(secondNum))} />
-        <Button title="-" onPress={() => setResult(firstNum - secondNum)} />
+        <Button title="+" onPress={() => calculate("+")} />
+        <Button title="-" onPress={() => calculate("-")} />
       </View>
+      <Text>History</Text>
+      <FlatList style={styles.flatliststyle} data={data}
+        renderItem={({ item }) => <Text>{item.calculation}</Text>}
+        keyExtractor={(item, index) => index.toString()} />
     </View>
   );
 }
@@ -27,18 +48,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   numberinput: {
     width: 250,
     borderColor: "gray",
     borderWidth: 1
   },
+  result: {
+    marginTop: 50
+  },
   buttoncontainer: {
     marginTop: 10,
     width: 75,
     flexDirection: "row",
     justifyContent: "space-between"
+  },
+  flatliststyle: {
+    height: 100,
+    paddingTop: 5
   }
 
 });
