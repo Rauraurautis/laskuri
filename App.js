a24cd95df16fa1e5b9dacfd5844681de64cdd2ac
unknown <rautanokka@gmail.com>
2022-01-18 13:15:09 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert, TextInput } from 'react-native';
+
 
 export default function App() {
+  const [firstNum, setFirstNum] = useState(0)
+  const [secondNum, setSecondNum] = useState(0)
+  const [result, setResult] = useState(null)
+
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{result ? `Result: ${result}` : ""}</Text>
+      <TextInput style={styles.numberinput} keyboardType={"numeric"} onChangeText={text => setFirstNum(text)} />
+      <TextInput style={styles.numberinput} keyboardType={"numeric"} onChangeText={text => setSecondNum(text)} />
+      <View style={styles.buttoncontainer}>
+        <Button title="+" onPress={() => setResult(Number(firstNum) + Number(secondNum))} />
+        <Button title="-" onPress={() => setResult(firstNum - secondNum)} />
+      </View>
     </View>
   );
 }
@@ -17,4 +29,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  numberinput: {
+    width: 250,
+    borderColor: "gray",
+    borderWidth: 1
+  },
+  buttoncontainer: {
+    marginTop: 10,
+    width: 75,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
+
 });
